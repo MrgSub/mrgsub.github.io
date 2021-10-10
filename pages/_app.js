1d93c65f7f0e71bf1f3a75aaab621fe4716d17ee
AJXB <ab0ud@live.com>
2021-10-10 15:48:45 -0700

    fix: GTM

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,21 @@ import "../styles/globals.css";
 import Head from "next/head";
 import React from "react";
 function MyApp({ Component, pageProps }) {
+	const renderGTMSnippet = () => {
+		return (
+			<script
+				dangerouslySetInnerHTML={{
+					__html: `
+						(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+						new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+						j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+						'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+						})(window,document,'script','dataLayer', "GTM-MTTP275");
+						`,
+				}}
+			/>
+		);
+	};
 	return (
 		<React.Fragment>
 			<Head>
@@ -10,24 +25,7 @@ function MyApp({ Component, pageProps }) {
 					href="https://fonts.googleapis.com/css2?family=Caveat:wght@400;500;600;700&family=Raleway:wght@300;500;700&display=swap"
 					rel="stylesheet"
 				/>
-				<script>
-					{(function (w, d, s, l, i) {
-						w[l] = w[l] || [];
-						w[l].push({
-							"gtm.start": new Date().getTime(),
-							event: "gtm.js",
-						});
-						var f = d.getElementsByTagName(s)[0],
-							j = d.createElement(s),
-							dl = l != "dataLayer" ? "&l=" + l : "";
-						j.async = true;
-						j.src =
-							"https://www.googletagmanager.com/gtm.js?id=" +
-							i +
-							dl;
-						f.parentNode.insertBefore(j, f);
-					})(window, document, "script", "dataLayer", "GTM-MTTP275")}
-				</script>
+				{renderGTMSnippet()}
 			</Head>
 			<noscript>
 				<iframe
